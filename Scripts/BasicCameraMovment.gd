94ec019c14154d23c57965e9c1ac80bb88ab42fb
Jose De Gouveia <dhgouveia@gmail.com>
2021-03-23 13:57:30 +0100

    Added mouse support to the basic camera movement

--- Scripts/BasicCameraMovment.gd
+++ Scripts/BasicCameraMovment.gd
@@ -1,30 +1,37 @@
-extends Camera
+extends KinematicBody
 
-export(int) var SPEED : int = 20
-export(int) var MAX_SPEED : int  = 30
+export(float) var SPEED : float = 20
+export(float) var MAX_SPEED : float  = 30
+export(float) var MOUSE_SENSIBILITY : float = 0.3
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta : float):
-	var speed : int = 0
+	var speed : float = 0
 	var direction : Vector3 = Vector3()
-		
-	# get the rotation of the camera
-	var aim : Basis = get_global_transform().basis
 	
 	# check input and change direction
 	if Input.is_action_pressed("move_forward"):
-		direction -= aim.z
+		direction -= transform.basis.z
 	if Input.is_action_pressed("move_backward"):
-		direction += aim.z
+		direction += transform.basis.z
 	if Input.is_action_pressed("move_left"):
-		direction -= aim.x
+		direction -= transform.basis.x
 	if Input.is_action_pressed("move_right"):
-		direction += aim.x
+		direction += transform.basis.x
 	if Input.is_action_pressed("move_sprint"):
 		speed = MAX_SPEED
 	else:
 		speed = SPEED
 
 	# move
-	direction.y = 0
 	direction = direction.normalized()
-	translate(direction * speed * delta)
+	move_and_slide(direction * speed, Vector3.UP)
+	
+func _input(event):
+	# mouse rotation
+	if event is InputEventMouseMotion:
+		self.rotate_y( deg2rad(-event.relative.x * MOUSE_SENSIBILITY) )
+		$Camera.rotate_x( deg2rad(-event.relative.y * MOUSE_SENSIBILITY) )
+		$Camera.rotation.x = clamp($Camera.rotation.x, deg2rad(-89), deg2rad(89))
